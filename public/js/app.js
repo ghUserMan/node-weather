79ea889edf6a4880075f5f92d47086905c987250
ghUserMan <apatiy.kirov@yandex.ru>
2022-01-27 02:38:30 +0300

    style for search error

--- public/js/app.js
+++ public/js/app.js
@@ -4,12 +4,14 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const errorMeggaseClass = 'error'
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
 
+    messageOne.classList.remove(errorMeggaseClass)
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
@@ -18,6 +20,7 @@ weatherForm.addEventListener('submit', (e) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
+                messageOne.classList.add(errorMeggaseClass)
             } else {
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast
